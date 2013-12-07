c5daf10ad5edc908b8a1d4345ff31667cf12e920
Andre Masella <andre@masella.name>
2013-12-07 07:41:53 -0800

    Make readline not exit on ^C

--- readline.c
+++ readline.c
@@ -5,11 +5,9 @@
 #include <readline/history.h>
 
 static sigjmp_buf jbuf;
-static bool had_data;
 
 static void handle_interrupt(
 	int sig) {
-	had_data = rl_end > 0;
 	siglongjmp(jbuf, 1);
 }
 
@@ -23,18 +21,14 @@ char *sane_readline(
 	new_action.sa_flags = 0;
 
 	sigaction(SIGINT, &new_action, &old_action);
-	do {
-		if (sigsetjmp(jbuf, 1)) {
+	if (sigsetjmp(jbuf, 1)) {
 #ifdef HAVE_SIGRELSE
-			sigrelse(SIGINT);
+		sigrelse(SIGINT);
 #endif
-			putchar('\n');
-			rl_reset_line_state();
-		} else {
-			had_data = false;
-			p = readline(prompt);
-		}
-	} while (had_data);
+		putchar('\n');
+		rl_reset_line_state();
+	}
+	p = readline(prompt);
 	sigaction(SIGHUP, &old_action, NULL);
 
 	return p;
