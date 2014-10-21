fba4574fcf76fdf0fded3e4bd6e4b3cb71c8b32e
Andre Masella <andre@masella.name>
2014-10-21 19:49:41 -0400

    Add compiler tests
    Change-Id: I4112cb38eb6c5a76bd98813f2b1330847ecc60cb

---
+++ platform/clr/test-harness.cs
@@ -0,0 +1,48 @@
+using Flabbergast;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Compiler {
+	public static void Main(string[] args) {
+		var uri = new Uri(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+		var directory = Path.GetDirectoryName(uri.LocalPath);
+		DoTests(Path.Combine(directory, "..", "..", "tests"));
+		DoTests(Path.Combine(directory, "tests"));
+	}
+	public static List<string> GetFiles(string root, string child) {
+		var files = new List<string>();
+		var path = Path.Combine(root, child);
+		if (Directory.Exists(path)) {
+			foreach (var file in Directory.GetFiles(path)) {
+				if (file.EndsWith(".flbgst")) {
+					files.Add(file);
+				}
+			}
+		}
+		return files;
+	}
+	public static void DoTests(string root) {
+		if (!Directory.Exists(root)) {
+			System.Console.WriteLine("Skipping non-existent directory: " + root);
+			return;
+		}
+		foreach (var file in GetFiles(root, "malformed")) {
+			System.Console.Write(Path.GetFileNameWithoutExtension(file) + "... ");
+			var parser = Parser.Open(file);
+			System.Console.WriteLine(AstNode.ParseFile(parser) == null ? "ok" : "FAIL");
+		}
+		foreach (var file in GetFiles(root, "errors")) {
+			System.Console.Write(Path.GetFileNameWithoutExtension(file) + "... ");
+			var parser = Parser.Open(file);
+			var ast = AstNode.ParseFile(parser);
+			System.Console.WriteLine(ast != null ? "ok" : "FAIL");
+		}
+		foreach (var file in GetFiles(root, "working")) {
+			System.Console.Write(Path.GetFileNameWithoutExtension(file) + "... ");
+			var parser = Parser.Open(file);
+			var ast = AstNode.ParseFile(parser);
+			System.Console.WriteLine(ast != null ? "ok" : "FAIL");
+		}
+	}
+}
