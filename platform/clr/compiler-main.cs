f29f6c89461ee198193c16bbc572bf96d8fd4963
Andre Masella <andre@masella.name>
2014-10-23 07:23:34 -0400

    Add a compiler front end
    Change-Id: I04febdbffbe88739673cb6f74f4a102ff7e3c49d

---
+++ platform/clr/compiler-main.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using Flabbergast;
+using NDesk.Options;
+
+public class Compiler {
+	public static void Main(string[] args) {
+		bool trace = false;
+		bool show_help = false;
+		var options = new OptionSet () {
+			{ "t|trace-parsing", "Produce a trace of the parse process.", v => trace = v != null },
+			{ "h|help",  "show this message and exit", v => show_help = v != null },
+		};
+
+		List<string> files;
+		try {
+			files = options.Parse (args);
+		} catch (OptionException e) {
+			Console.Write (System.AppDomain.CurrentDomain.FriendlyName + ": ");
+			Console.WriteLine (e.Message);
+			Console.WriteLine ("Try `" + System.AppDomain.CurrentDomain.FriendlyName + " --help' for more information.");
+			return;
+		}
+
+		if (show_help) {
+			Console.WriteLine ("Usage: " + System.AppDomain.CurrentDomain.FriendlyName + " files ...");
+			Console.WriteLine ("Compile a Flabbergast file to native CLR.");
+			Console.WriteLine ();
+			Console.WriteLine ("Options:");
+			options.WriteOptionDescriptions (Console.Out);
+			return;
+		}
+		if (files.Count == 0) {
+			Console.WriteLine ("Perhaps you wish to compile some source files?");
+			return;
+		}
+		foreach (var filename in files) {
+			var parser = Parser.Open(filename);
+			parser.Trace = trace;
+			if (AstNode.ParseFile(parser) == null) {
+				System.Console.WriteLine(parser.Message);
+			}
+		}
+	}
+}
