8c6e6bddafcd58645210166c5d897e97be0e0222
Andre Masella <andre@masella.name>
2015-01-15 20:11:01 -0500

    Add method to compare Stringish
    Change-Id: Ia51900c73b652ef08b95c6bd4d445162d170cb0d

--- platform/clr/stringish.cs
+++ platform/clr/stringish.cs
@@ -1,10 +1,41 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Flabbergast {
-public abstract class Stringish {
+public abstract class Stringish : IComparable<Stringish> {
 	public abstract int Length { get; }
 	public abstract void Write(TextWriter writer);
+	public int CompareTo(Stringish other) {
+		var this_stream = this.Stream();
+		var other_stream = this.Stream();
+		if (!this_stream.MoveNext()) {
+			return other_stream.MoveNext() ? -1 : 0;
+		}
+		if (!other_stream.MoveNext()) {
+			return 1;
+		}
+		var this_offset = 0;
+		var other_offset = 0;
+		var result = 0;
+		while (result == 0) {
+			if (UpdateIterator(this_stream, ref this_offset)) break;
+			if (UpdateIterator(other_stream, ref other_offset)) break;
+			var length = Math.Min(this_stream.Current.Length - this_offset, other_stream.Current.Length - other_offset);
+			result = String.Compare(this_stream.Current, this_offset, other_stream.Current, other_offset, length, StringComparison.CurrentCulture);
+			this_offset += length;
+			other_offset += length;
+		}
+		return result;
+	}
+	private bool UpdateIterator(IEnumerator<string> enumerator, ref int offset) {
+		while (enumerator.Current.Length <= offset) {
+			if (!enumerator.MoveNext()) return true;
+			offset = 0;
+		}
+		return false;
+	}
+	public abstract IEnumerator<string> Stream();
 	public override string ToString() {
 		var writer = new StringWriter();
 		this.Write(writer);
@@ -17,6 +48,9 @@ public class SimpleString : Stringish {
 	public override int Length { get { return str.Length; } }
 	public SimpleString(string str) {
 		this.str = str;
+	}
+	public override IEnumerator<string> Stream() {
+		yield return str;
 	}
 	public override void Write(TextWriter writer) {
 		writer.Write(str);
@@ -32,6 +66,12 @@ public class ConcatString : Stringish {
 		this.head = head;
 		this.tail = tail;
 		this.chars = head.Length + tail.Length;
+	}
+	public override IEnumerator<string> Stream() {
+		var head_enumerator = head.Stream();
+		while(head_enumerator.MoveNext()) yield return head_enumerator.Current;
+		var tail_enumerator = tail.Stream();
+		while(tail_enumerator.MoveNext()) yield return tail_enumerator.Current;
 	}
 	public override void Write(TextWriter writer) {
 		head.Write(writer);
