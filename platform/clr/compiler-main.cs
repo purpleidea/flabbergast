21b78a567eb5bf4f9fc7bd200d2ee06785fc81fd
Andre Masella <andre@masella.name>
2015-01-23 18:59:09 -0500

    Change all backticks and apostrophes to Unicode quotation marks
    Change-Id: I35db4487d43dcd22b982df7e0ec858770bd522eb

--- platform/clr/compiler-main.cs
+++ platform/clr/compiler-main.cs
@@ -8,10 +8,10 @@ public class ConsoleCollector : ErrorCollector {
 		Console.WriteLine("{0}:{1}:{2}-{3}:{4}: Expression has conflicting types: {5} versus {6}.", where.FileName, where.StartRow, where.StartColumn, where.EndRow, where.EndColumn, new_type, existing_type);
 	}
 	public void ReportTypeError(Environment environment, string name, Type new_type, Type existing_type) {
-		Console.WriteLine("{0}:{1}:{2}-{3}:{4}: Lookup for `{5}' has conflicting types: {6} versus {7}.", environment.FileName, environment.StartRow, environment.StartColumn, environment.EndRow, environment.EndColumn, name, new_type, existing_type);
+		Console.WriteLine("{0}:{1}:{2}-{3}:{4}: Lookup for “{5}” has conflicting types: {6} versus {7}.", environment.FileName, environment.StartRow, environment.StartColumn, environment.EndRow, environment.EndColumn, name, new_type, existing_type);
 	}
 	public void ReportForbiddenNameAccess(Environment environment, string name) {
-		Console.WriteLine("{0}:{1}:{2}-{3}:{4}: Lookup for `{5}' is forbidden.", environment.FileName, environment.StartRow, environment.StartColumn, environment.EndRow, environment.EndColumn, name);
+		Console.WriteLine("{0}:{1}:{2}-{3}:{4}: Lookup for “{5}” is forbidden.", environment.FileName, environment.StartRow, environment.StartColumn, environment.EndRow, environment.EndColumn, name);
 	}
 	public void RawError(AstNode where, string message) {
 		Console.WriteLine("{0}:{1}:{2}-{3}:{4}: {5}", where.FileName, where.StartRow, where.StartColumn, where.EndRow, where.EndColumn, message);
@@ -32,7 +32,7 @@ public class Compiler {
 		} catch (OptionException e) {
 			Console.Write (System.AppDomain.CurrentDomain.FriendlyName + ": ");
 			Console.WriteLine (e.Message);
-			Console.WriteLine ("Try `" + System.AppDomain.CurrentDomain.FriendlyName + " --help' for more information.");
+			Console.WriteLine ("Try “" + System.AppDomain.CurrentDomain.FriendlyName + " --help” for more information.");
 			return;
 		}
 
