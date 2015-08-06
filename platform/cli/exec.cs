1a8cbbda1800ebf6ba20e4220488f741dcdd58eb
Andre Masella <andre@masella.name>
2015-08-05 22:45:45 -0400

    Attempt to glue Computations to C#'s async/away system
    This might not work as intended or be useful, since the Java port can never
    have this.
    
    Change-Id: I8ff2d437b15bb447226758417b08b71527de81ce

--- platform/cli/exec.cs
+++ platform/cli/exec.cs
@@ -527,4 +527,92 @@ namespace Flabbergast {
 			return false;
 		}
 	}
+
+	/**
+	 * Adapt C#'s async/await system to Flabbergast's TaskMaster.
+	 */
+	public abstract class AsyncComputation : Computation {
+		private class AsyncResult : IAsyncResult {
+				private readonly object state;
+				private readonly AsyncCallback callback;
+				private readonly AsyncComputation owner;
+				private ManualResetEvent reset_event = new ManualResetEvent(false);
+				private int interlock = 2;
+
+				public AsyncResult(AsyncComputation owner, AsyncCallback callback, object state) {
+					this.owner = owner;
+					this.callback = callback;
+					this.state = state;
+				}
+
+        public object AsyncState { get { return state; } }
+
+        public WaitHandle AsyncWaitHandle { get { return reset_event; } }
+
+        public bool CompletedSynchronously { get; internal set; }
+
+        public bool HasCallback { get { return this.callback != null; } }
+
+        public bool IsCompleted { get { return Result != null; } }
+
+				internal object Result = null;
+
+				public void Continue() {
+					callback(this);
+				}
+
+				public void Prepare(Computation c) {
+					c.Notify(result => {
+						this.Result = result;
+						reset_event.Set();
+						if (Interlocked.Decrement(ref interlock) == 0) {
+							owner.task_master.Slot(owner);
+						}
+					});
+					if (Interlocked.Decrement(ref interlock) == 0) {
+						CompletedSynchronously = true;
+						Continue();
+					}
+				}
+		}
+
+		private AsyncResult state = null;
+
+		public AsyncComputation(TaskMaster task_master) : base(task_master) {
+		}
+
+		/**
+		 * Asynchronously wait for the result of a computation.
+		 *
+		 * Call `await Go(computation)` to wait for the result.
+		 */
+		public IAsyncResult BeginGo(Computation c, AsyncCallback callback, object asyncState) {
+			state = new AsyncResult(this, callback, asyncState);
+			state.Prepare(c);
+			return state;
+		}
+
+		public object EndGo(IAsyncResult r)
+		{
+			AsyncResult ar = r as AsyncResult;
+			return ar.Result;
+		}
+
+		/**
+		 * Override with a user computation.
+		 */
+		protected abstract void RunAsync();
+
+		protected override bool Run() {
+			if (state == null) {
+				Action action = RunAsync;
+				action.BeginInvoke((ar) => { action.EndInvoke(ar); if (result != null) WakeupListeners(); }, this);
+			} else {
+				var x = state;
+				state = null;
+				x.Continue();
+			}
+			return false;
+		}
+	}
 }
