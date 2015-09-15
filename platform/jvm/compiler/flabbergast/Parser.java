d93501814c3f6eb3e584df790d7aa79f926ccbf6
Andre Masella <andre@masella.name>
2015-09-15 07:22:45 -0400

    Force file parsing to UTF-8 in Java
    Change-Id: Ic2fc2858e25a2453580a27daa8d2491cb2b15e4b

--- platform/jvm/compiler/flabbergast/Parser.java
+++ platform/jvm/compiler/flabbergast/Parser.java
@@ -1,6 +1,7 @@
 package flabbergast;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
@@ -290,7 +291,7 @@ public class Parser {
 	 */
 	public static Parser open(String filename) throws IOException {
 		return new Parser(filename, new String(Files.readAllBytes(Paths
-				.get(filename))));
+				.get(filename)), StandardCharsets.UTF_8));
 	}
 
 	/**
